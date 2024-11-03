0aa68504f6da732c8b686da262651068ee990ee0
Peter Sideris <petersid2022@gmail.com>
2024-11-03 17:33:16 +0200

    Updated to include the multiple header files

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -xe
 CC="gcc"
 CFLAGS="-std=c99 -pedantic -Wall -Wextra -O0 -ggdb3 -fsanitize=address"
 IFLAGS="-Isrc"
-SRC="./src/main.c"
+SRC="./src/file.c ./src/instructions.c ./src/utils.c ./src/main.c"
 
 $CC $IFLAGS $CFLAGS $SRC
 
