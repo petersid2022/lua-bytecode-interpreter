888782041f6172a09771fc8c8734031a6f143b6f
Peter Sideris <petersid2022@gmail.com>
2024-11-05 15:31:44 +0200

    Fixed print_binary byte order

--- src/utils.c
+++ src/utils.c
@@ -1,48 +1,55 @@
 #include <assert.h>
-#include <stdlib.h>
 #include <stdint.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include "colors.h"
 #include "file.h"
 #include "utils.h"
-#include "colors.h"
 
-uint8_t *poke_bytes(FILE_BYTES *file_bytes, const size_t amount) {
-	assert(amount > 0);
+uint8_t *poke_bytes(s_Filebytes *file_bytes, const size_t amount) {
+    assert(amount > 0);
 
-	uint8_t *bytes = calloc(amount, sizeof(uint8_t));
-	if (bytes == NULL) {
-		fprintf(stderr, "error: calloc() failed\n");
-		return NULL;
-	}
+    // Check that we have enough bytes left to read
+    if (file_bytes->cursor + amount > file_bytes->length) {
+        fprintf(stderr, "error: not enough bytes to read\n");
+        return NULL;
+    }
 
-	memcpy(bytes, file_bytes->bytes + file_bytes->cursor, amount);
+    uint8_t *bytes = calloc(amount, sizeof(uint8_t));
+    if (bytes == NULL) {
+        fprintf(stderr, "error: calloc() failed\n");
+        return NULL;
+    }
 
-	file_bytes->cursor += amount;
-
-	return bytes;
+    memcpy(bytes, file_bytes->bytes + file_bytes->cursor, amount);
+    file_bytes->cursor += amount;
+    return bytes;
 }
 
-uint8_t poke_next_byte(FILE_BYTES *file_bytes) {
-	uint8_t out = (*file_bytes).bytes[file_bytes->cursor];
-	file_bytes->cursor++;
-	return out;
+uint8_t poke_next_byte(s_Filebytes *file_bytes) {
+    uint8_t out = (*file_bytes).bytes[file_bytes->cursor];
+    file_bytes->cursor++;
+    return out;
 }
 
-void skip_bytes(FILE_BYTES *file_bytes, const size_t n) {
-	(*file_bytes).cursor += n;
+void skip_bytes(s_Filebytes *file_bytes, const size_t n) {
+    (*file_bytes).cursor += n;
 }
 
 void print_binary(const unsigned long value, const int width) {
-	for (int i = 0; i < width; ++i)
-		putchar((value & (1UL << i)) ? '1' : '0');
+    for (int i = 0; i < width; ++i)
+        putchar((value & (1UL << (width - i - 1))) ? '1' : '0');
+    printf("\n");
 }
 
-void print_full_hex(FILE_BYTES *const *file_bytes) {
-	for (size_t i = 0; i < (*file_bytes)->length; ++i) {
-		if (i % 2 == 0 && i != 0) printf(" ");
-		if (i % 16 == 0 && i != 0) printf("\n");
-		printf(RED "%.2x" RESET, (*file_bytes)->bytes[i]);
-	}
+void print_full_hex(s_Filebytes *const *file_bytes) {
+    for (size_t i = 0; i < (*file_bytes)->length; ++i) {
+        if (i % 2 == 0 && i != 0)
+            printf(" ");
+        if (i % 16 == 0 && i != 0)
+            printf("\n");
+        printf(RED "%.2x" RESET, (*file_bytes)->bytes[i]);
+    }
 }
--- src/utils.h
+++ src/utils.h
@@ -3,11 +3,11 @@
 
 #include "file.h"
 
-void print_full_hex(FILE_BYTES *const *file_bytes);
+void print_full_hex(s_Filebytes *const *file_bytes);
 void print_binary(const unsigned long value, const int width);
 
-uint8_t poke_next_byte(FILE_BYTES *file_bytes);
-void skip_bytes(FILE_BYTES *file_bytes, const size_t n);
-uint8_t *poke_bytes(FILE_BYTES *file_bytes, const size_t amount);
+uint8_t  poke_next_byte(s_Filebytes *file_bytes);
+void     skip_bytes(s_Filebytes *file_bytes, const size_t n);
+uint8_t *poke_bytes(s_Filebytes *file_bytes, const size_t amount);
 
-#endif      /*   UTILS_H__   */
+#endif /*   UTILS_H__   */
