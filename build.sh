cd41ea072fd243808fd7f18ec20577d5f8ca255b
Peter Sideris <petersid2022@gmail.com>
2024-11-05 13:56:39 +0200

    Updated to include final executable name

--- build.sh
+++ build.sh
@@ -6,9 +6,8 @@ CC="gcc"
 CFLAGS="-std=c99 -pedantic -Wall -Wextra -O0 -ggdb3 -fsanitize=address"
 IFLAGS="-Isrc"
 SRC="./src/file.c ./src/instructions.c ./src/utils.c ./src/main.c"
+OUTPUT="./lbi"
 
-$CC $IFLAGS $CFLAGS $SRC
+$CC $IFLAGS $CFLAGS -o $OUTPUT $SRC
 
-./a.out lua/luac.out
-
-rm ./a.out
+$OUTPUT lua/luac.out
