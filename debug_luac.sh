01c6f746ae5f1f3f8f3a39d017b62f586881c4b5
Peter Sideris <petersid2022@gmail.com>
2024-11-08 09:42:18 +0200

    Untar the tarball and modify the Makefile before building

--- debug_luac.sh
+++ debug_luac.sh
@@ -5,12 +5,15 @@ set -xe
 LUAC_PATH="./lua/lua-5.4.7/src/luac"
 
 build_luac() {
-    make -C ./lua/lua-5.4.7 -j$(nproc) -B
+  mkdir -p ./lua/lua-5.4.7
+  tar -xvzf ./lua/lua-5.4.7.tar.gz  -C ./lua
+  sed -i 's/^CFLAGS=.*/CFLAGS= -ggdb3 -O0 -Wall -Wextra -DLUA_COMPAT_5_3 $(SYSCFLAGS) $(MYCFLAGS)/' ./lua/lua-5.4.7/src/Makefile
+  make -C ./lua/lua-5.4.7 -j$(nproc) -B
 }
 
 if [ ! -f "$LUAC_PATH" ]; then
-    echo "$LUAC_PATH does not exist. Building..."
-    build_luac
+  echo "$LUAC_PATH does not exist. Building..."
+  build_luac
 fi
 
 gdb -tui "$LUAC_PATH"
