314038ced373d74093df29b3341ffb89bcd9c1df
Peter Sideris <petersid2022@gmail.com>
2025-02-14 23:31:46 +0200

    fixed formatting and fibonacci code

--- fibonacci/fib.lua
+++ fibonacci/fib.lua
@@ -1,11 +1,13 @@
 function fib(n)
-	if n == 1 or n == 2 then
-		return 1
-	end
-
-	return fib(n - 1) + fib(n - 2)
+    if n <= 0 then
+        return 0
+    elseif n == 1 then
+        return 1
+    else
+        return fib(n - 1) + fib(n - 2)
+    end
 end
 
-for i=1,40 do
-	print(fib(i))
+for i = 1, 40 do
+    print(fib(i))
 end
