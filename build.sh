6b8b8bcdcab72d4b2ea823550379d4991163e5c1
Peter Sideris <petersid2022@gmail.com>
2025-02-20 19:27:02 +0200

    changed gcc to clang for better compatibility with musl

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-CC="gcc"
+CC="clang"
 CFLAGS="-std=c99 -pedantic -Wall -Wextra -O0 -ggdb3 -fsanitize=address"
 IFLAGS="-Isrc"
 SRC="./src/file.c ./src/instructions.c ./src/utils.c ./src/main.c"
